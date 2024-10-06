92a55312267db521c7f9e88d55557518bb4abef0
Fadilian Risdianto <fadilian1124@student.ub.ac.id>
2024-10-06 13:56:08 +0700

    add library

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import UserList from "./components/UserList";
+import AddUser from "./components/AddUser";
+import EditUser from "./components/EditUser";
+import BookList from "./components/BookList"; 
+import AddBook from "./components/AddBook"; 
+import EditBook from "./components/EditBook"; 
+import BorrowingList from "./components/BorrowingList";
+import AddBorrowing from "./components/AddBorrowing";
+import EditBorrowing from "./components/EditBorrowing";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/user" element={<UserList />} />
+        <Route path="/user/add" element={<AddUser />} />
+        <Route path="/user/edit/:id" element={<EditUser />} />
+        <Route path="/book" element={<BookList />} /> 
+        <Route path="/book/add" element={<AddBook />} /> 
+        <Route path="/book/edit/:id" element={<EditBook />} /> 
+        <Route path="/borrowing" element={<BorrowingList />} />
+        <Route path="/borrowing/add" element={<AddBorrowing />} />
+        <Route path="/borrowing/edit/:id" element={<EditBorrowing />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
